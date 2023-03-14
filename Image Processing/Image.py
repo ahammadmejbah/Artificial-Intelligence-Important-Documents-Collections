b073100312fb7397e60eb064db866ebe99549049
Mejbah Ahammad <56669333+ahammadmejbah@users.noreply.github.com>
2023-03-15 02:08:20 +0600

    Initial commit

--- Image Processing/Image.py
+++ Image Processing/Image.py
@@ -2,7 +2,7 @@ import os
 from random import randint
 
 for i in range(1, 365):
-    for j in range(0, randint(1, 100)):
+    for j in range(0, randint(1, 2)):
         d = str(i) + "days ago"
         with open('file.txt', 'a') as file:
             file.write(d)
