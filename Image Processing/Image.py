475b923ef737fc0efd6d25807558cd8fcd504dd4
Mejbah Ahammad <56669333+ahammadmejbah@users.noreply.github.com>
2023-03-15 01:23:29 +0600

    Initial Commit

---
+++ Image Processing/Image.py
@@ -0,0 +1,13 @@
+import os
+from random import randint
+
+for i in range(1, 365):
+    for j in range(0, randint(1, 100)):
+        d = str(i) + "days ago"
+        with open('file.txt', 'a') as file:
+            file.write(d)
+
+        os.system('git add .')
+        os.system("git commit -m'%s'"%d)
+
+os.system('git push -u origin main')
