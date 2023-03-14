f9b8843e12148ced07c883087f4ca166d411fbd6
Mejbah Ahammad <56669333+ahammadmejbah@users.noreply.github.com>
2023-03-15 02:19:01 +0600

    Initial

--- Image Processing/Image.py
+++ Image Processing/Image.py
@@ -1,13 +1,17 @@
-import os
-from random import randint
+def mean_squred_error(image01, image02):
+    
+    """ 
+ error = np.sum((image01.astype("float") - image02.astype("float"))**2)
+    error = error/float(image01.shape[0] * image02.shape[1])
+    return error
 
-for i in range(1, 365):
-    for j in range(0, randint(1, 2)):
-        d = str(i) + "days ago"
-        with open('file.txt', 'a') as file:
-            file.write(d)
+"""
+    
+    error = np.sum((image01.astype("float") - image02.astype("float"))**2)
+    error = error/float(image01.shape[0] * image02.shape[1])
+    return error
 
-        os.system('git add .')
-        os.system('git commit --date="' + d + '" -m "commit"')
-
-os.system('git push -u origin main')
+def image_comparision(image01, image02):
+    m = mean_squred_error(image01, image02)
+    s = ssim(image01, image02)
+    print("Mean Squared Error is {}\nStructural Similarity Index Measure is: {}".format(m, s))
