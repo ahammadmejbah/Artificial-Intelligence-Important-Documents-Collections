692a115418103cabe328a5a9fc7401c5707ee81b
Mejbah Ahammad <56669333+ahammadmejbah@users.noreply.github.com>
2023-03-15 02:19:50 +0600

    update

--- Image Processing/Image.py
+++ Image Processing/Image.py
@@ -11,3 +11,7 @@ def mean_squred_error(image01, image02):
     error = error/float(image01.shape[0] * image02.shape[1])
     return error
 
+def image_comparision(image01, image02):
+    m = mean_squred_error(image01, image02)
+    s = ssim(image01, image02)
+    print("Mean Squared Error is {}\nStructural Similarity Index Measure is: {}".format(m, s))
